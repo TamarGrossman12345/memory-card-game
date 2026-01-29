3616318113be665a57c6624a514a0cfa2b07c707
Tamar Grossman <tamar.yust@gmail.com>
2026-01-29 21:35:28 +0200

    refactoring the loading page styles

--- src/pages/LoadingPage.tsx
+++ src/pages/LoadingPage.tsx
@@ -1,7 +1,7 @@
 import { useNavigate } from "react-router-dom";
 import myLoadingGif from "../assets/background/loading.gif";
 import "../styles/loadingPage.css";
-import { Typography } from "@mui/material";
+import { Box, Typography } from "@mui/material";
 import { useEffect } from "react";
 
 const LoadingPage = () => {
@@ -15,28 +15,53 @@ const LoadingPage = () => {
   }, [navigate]);
 
   return (
-    <div className="loading-overlay">
-      <img
-        className="loading-background-gif"
-        alt="background"
-        src={myLoadingGif}
-      />
-
-      <div className="loading-content">
-        <Typography
-          variant="h2"
+    <>
+      <Box
+        sx={{
+          position: "fixed",
+          top: 0,
+          left: 0,
+          width: "100vw",
+          height: "100vh",
+          display: "flex",
+          zIndex: -1,
+          justifyContent: "center",
+          alignItems: "center",
+          overflow: "hidden",
+        }}
+      >
+        <Box
+          component="img"
+          src={myLoadingGif}
           sx={{
-            fontFamily: "'MedievalSharp', cursive",
-            color: "#e1e85b",
-            textAlign: "center",
-            textShadow: "2px 2px 10px rgba(162, 194, 227, 0.5)",
-            letterSpacing: "2px",
+            position: "absolute",
+            width: "100%",
+            height: "100%",
+            zIndex: -1,
+            objectFit: "cover",
+            filter: "blur(4px) brightness(0.7)",
+            transform: "scale(1)",
           }}
-        >
-          Loading...
-        </Typography>
-      </div>
-    </div>
+        ></Box>
+    
+          <Typography
+            variant="h2"
+            sx={{
+              position: "relative",
+              animation: "pulse 1.5s infinite ease-in-out",
+              zIndex: 1,
+              fontFamily: "'MedievalSharp', cursive",
+              color: "#e1e85b",
+              textAlign: "center",
+              textShadow: "2px 2px 10px rgba(162, 194, 227, 0.5)",
+              letterSpacing: "2px",
+            }}
+          >
+            Loading...
+          </Typography>
+    
+      </Box>
+    </>
   );
 };
 
